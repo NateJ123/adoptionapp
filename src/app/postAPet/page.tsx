9b4a089d3e70603b7260c7148ba3b0f946ffb412
Andrew Murdoch <132223184+alm26920@users.noreply.github.com>
2024-11-22 18:00:57 -0500

    posting a pet implemented

--- src/app/postAPet/page.tsx
+++ src/app/postAPet/page.tsx
@@ -1,9 +1,31 @@
-"use client"
+"use client";
+
+import { useState } from "react";
 import Poster from "../components/Poster";
+
+type Pet = {
+    id: number;
+    name: string;
+    imageUrl: string;
+    age: string;
+    shelter: string;
+    description: string;
+};
+
+type PetProps = {
+    addPet: (pet: Pet) => void;
+};
+
 export default function Home() {
+    const [pets, setPets] = useState<Pet[]>([]);
+
+    const addPet = (newPet: Pet) => {
+        setPets((prevPets) => [...prevPets, newPet]);
+    };
+
     return (
         <div>
-            <Poster/>
+            <Poster addPet={addPet} />
         </div>
     );
-};
+}
